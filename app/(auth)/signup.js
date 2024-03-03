9331b1b25a2192f9ded157379dda5f85ab68ed17
Alec Schrader <alecschrader@gmail.com>
2024-03-03 11:10:20 -0500

    create sign up page

--- app/(auth)/signup.js
+++ app/(auth)/signup.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import { Link } from 'expo-router';
 
-import { Alert, StyleSheet, View } from 'react-native'
-import { Button, TextInput } from 'react-native-paper'
+import { View } from 'react-native'
+import { Button, TextInput, Appbar } from 'react-native-paper'
 import { supabase } from '../../utils/supabase'
 import { styles } from '../../utils/styles'
 import { router } from 'expo-router';
@@ -12,21 +12,29 @@ export default function Login() {
     const [password, setPassword] = useState('')
     const [loading, setLoading] = useState(false)
 
-    async function signInWithEmail() {
-        setLoading(true)
-        const { error } = await supabase.auth.signInWithPassword({
-          email: email,
-          password: password,
+    async function signUpWithEmail() {
+        const {
+            data: { session },
+            error,
+        } = await supabase.auth.signUp({
+            email: email,
+            password: password,
         })
-    
-        if (error) Alert.alert(error.message)
-        setLoading(false)
 
+        if (error) console.error(error)
         router.replace('/')
+
     }
 
     return (
         <View style={styles.container}>
+            <Appbar.Header>
+                <Link href="/login" asChild>
+                    <Appbar.BackAction onPress={() => { }} />
+                </Link>
+                
+                <Appbar.Content title="Sign Up" />
+            </Appbar.Header>
             <View style={[styles.verticallySpaced, styles.mt20]}>
                 <TextInput
                     label="Email"
@@ -47,12 +55,7 @@ export default function Login() {
                 />
             </View>
             <View style={[styles.verticallySpaced, styles.mt20]}>
-                <Button disabled={loading} onPress={() => signInWithEmail()} mode="contained">Sign In</Button>
-            </View>
-            <View style={styles.verticallySpaced}>
-                <Link href="/signup" asChild>
-                    <Button>Sign up</Button>
-                </Link>
+                <Button disabled={loading} onPress={() => signUpWithEmail()} mode="contained">Sign Up</Button>
             </View>
         </View>
     )
